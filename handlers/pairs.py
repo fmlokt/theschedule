29ef9bda4b5ae8050dadf9dfaff8e38e3978c54a
Nikolay Kalinin <nakalinin@gmail.com>
2015-04-04 00:05:08 +0300

    move objects and handlers to separate directory, fix sorting

---
+++ handlers/pairs.py
@@ -0,0 +1,26 @@
+import os
+import datetime
+
+import webapp2
+
+from objects.pair import *
+from environment import JINJA_ENVIRONMENT
+
+class ShowPairs(webapp2.RequestHandler):
+	def get(self):
+		pairs_qry = ScheduledPair.query().order(ScheduledPair.date, ScheduledPair.start_time)
+		template = JINJA_ENVIRONMENT.get_template('templates/pairs.html')
+		render_data = { 'pairs' : []}
+		for pair in pairs_qry:
+			render_data['pairs'].append(pair)
+		self.response.write(template.render(render_data))
+	
+	def post(self):
+		classname = self.request.get('classname')
+		year = int(self.request.get('year'))
+		month = int(self.request.get('month'))
+		date = int(self.request.get('date'))
+		hour = int(self.request.get('hour'))
+		minute = int(self.request.get('minute'))
+		pair = ScheduledPair(classname=classname, date=datetime.date(year, month, date), start_time=datetime.time(hour, minute))
+		pair.put()
--- main.py
+++ main.py
@@ -1,44 +1,12 @@
 import os
-import datetime
 
-import webapp2
 import jinja2
+import webapp2
 
-from pair import *
-
-JINJA_ENVIRONMENT = jinja2.Environment(
-    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
-    extensions=['jinja2.ext.autoescape']
-)
-
-
-class MainPage(webapp2.RequestHandler):
-    def get(self):
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('Hello, World!')
-
-class ShowPairs(webapp2.RequestHandler):
-	def get(self):
-		pairs_qry = ScheduledPair.query().order(ScheduledPair.date, ScheduledPair.date)
-		template = JINJA_ENVIRONMENT.get_template('templates/pair.html')
-		render_data = { 'pairs' : []}
-		for pair in pairs_qry:
-			render_data['pairs'].append(pair)
-		self.response.write(template.render(render_data))
-	
-	def post(self):
-		classname = self.request.get('classname')
-		year = int(self.request.get('year'))
-		month = int(self.request.get('month'))
-		date = int(self.request.get('date'))
-		hour = int(self.request.get('hour'))
-		minute = int(self.request.get('minute'))
-		pair = ScheduledPair(classname=classname, date=datetime.date(year, month, date), start_time=datetime.time(hour, minute))
-		pair.put()
-
+import environment
+from handlers import pairs
 
 
 application = webapp2.WSGIApplication([
-    ('/', MainPage),
-    ('/pairs', ShowPairs),
+    ('/pairs', pairs.ShowPairs),
 ], debug=True)
