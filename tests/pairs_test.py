1852cac3ed44a6d6adf0a9172fd9597f4aa7d99a
Nikolay Kalinin <nakalinin@gmail.com>
2015-04-15 23:24:35 +0300

    added some pair_tests

--- tests/pairs_test.py
+++ tests/pairs_test.py
@@ -44,16 +44,32 @@ class PairsTest(unittest2.TestCase):
     def test_create_pair(self):
         response = PairsTest.make_request('/new_pair', 'GET')
         self.assertEqual(response.status_int, 200)
-        pair = ScheduledPair(classname='Math',
-                             date=datetime.date(2015, 4, 14),
-                             start_time=datetime.time(9, 40),
-                             task='some_task')
-        response = PairsTest.post_pair(pair)
+        pair1 = ScheduledPair(classname='Math',
+                              date=datetime.date(2015, 4, 14),
+                              start_time=datetime.time(9, 40),
+                              task='some_task')
+        response = PairsTest.post_pair(pair1)
         self.assertEqual(response.status_int, 302)
         pairs_list = ScheduledPair.query().fetch(2)
         self.assertEqual(len(pairs_list), 1)
         added_pair = pairs_list[0]
-        self.check_pair_fields(added_pair, pair)
+        self.check_pair_fields(added_pair, pair1)
+        response = PairsTest.make_request('/pairs', 'GET')
+        self.assertEqual(response.status_int, 200)
+        pair2 = ScheduledPair(classname='Math2',
+                              date=datetime.date(2015, 4, 15),
+                              start_time=datetime.time(9, 40),
+                              task='some_task')
+        response = PairsTest.post_pair(pair2)
+        self.assertEqual(response.status_int, 302)
+        pairs_list = ScheduledPair.query().fetch(3)
+        self.assertEqual(len(pairs_list), 2)
+        added_pair1 = pairs_list[0]
+        added_pair2 = pairs_list[1]
+        if added_pair1.classname != pair1.classname:
+            swap(added_pair1, added_pair2)
+        self.check_pair_fields(added_pair1, pair1)
+        self.check_pair_fields(added_pair2, pair2)
         response = PairsTest.make_request('/pairs', 'GET')
         self.assertEqual(response.status_int, 200)
 
@@ -80,14 +96,37 @@ class PairsTest(unittest2.TestCase):
         response = PairsTest.make_request('/pairs', 'GET')
         self.assertEqual(response.status_int, 200)
 
+    def test_delete_pair(self):
+        pair1 = ScheduledPair(classname='Math1',
+                              date=datetime.date(2015, 4, 14),
+                              start_time=datetime.time(10, 40),
+                              task='some_task')
+        pair2 = ScheduledPair(classname='Math2',
+                              date=datetime.date(2015, 4, 15),
+                              start_time=datetime.time(9, 40),
+                              task='some_task')
+        PairsTest.post_pair(pair1)
+        PairsTest.post_pair(pair2)
+        pairs_list = ScheduledPair.query().fetch(2)
+        added_pair1 = pairs_list[0]
+        added_pair2 = pairs_list[1]
+        response = PairsTest.make_request('/delete_pair?key=' +
+                                          added_pair1.key.urlsafe() +
+                                          '&return_url=/pairs', 'GET')
+        self.assertEqual(response.status_int, 302)
+        pairs_list = ScheduledPair.query().fetch(2)
+        self.assertEqual(len(pairs_list), 1)
+        remained_pair = pairs_list[0]
+        self.assertEqual(remained_pair, added_pair2)
+
     def test_show_pairs(self):
         response = PairsTest.make_request('/pairs', 'GET')
         self.assertEqual(response.status_int, 200)
         self.assertEqual(response.body.count('<tr>'), 0)
         pair1 = ScheduledPair(classname='Math1',
-                             date=datetime.date(2015, 4, 14),
-                             start_time=datetime.time(10, 40),
-                             task='some_task')
+                              date=datetime.date(2015, 4, 14),
+                              start_time=datetime.time(10, 40),
+                              task='some_task')
         pair2 = ScheduledPair(classname='Math2',
                               date=datetime.date(2015, 4, 15),
                               start_time=datetime.time(9, 40),
@@ -102,6 +141,35 @@ class PairsTest(unittest2.TestCase):
         response = PairsTest.make_request('/pairs', 'GET')
         self.assertEqual(response.status_int, 200)
         self.assertEqual(response.body.count('<tr>'), 3)
+        self.assertNotEqual(response.body.find('Math1'), -1)
+        self.assertNotEqual(response.body.find('Math2'), -1)
+        self.assertNotEqual(response.body.find('Math3'), -1)
+        self.assertLess(response.body.find('Math1'), response.body.find('Math2'))
+        self.assertLess(response.body.find('Math2'), response.body.find('Math3'))
+
+    def test_show_pairs(self):
+        response = PairsTest.make_request('/', 'GET')
+        self.assertEqual(response.status_int, 200)
+        self.assertEqual(response.body.count('<tr>'), 1)
+        pair1 = ScheduledPair(classname='Math1',
+                              date=datetime.date.today(),
+                              start_time=datetime.time(9, 10),
+                              task='some_task')
+        pair2 = ScheduledPair(classname='Math2',
+                              date=datetime.date.today(),
+                              start_time=datetime.time(10, 40),
+                              task='some_task')
+        pair3 = ScheduledPair(classname='Math3',
+                              date=datetime.date.today() +
+                              datetime.timedelta(days=1),
+                              start_time=datetime.time(9, 40),
+                              task='some_task')
+        PairsTest.post_pair(pair2)
+        PairsTest.post_pair(pair3)
+        PairsTest.post_pair(pair1)
+        response = PairsTest.make_request('/', 'GET')
+        self.assertEqual(response.status_int, 200)
+        self.assertEqual(response.body.count('<tr>'), 4)
         self.assertNotEqual(response.body.find('Math1'), -1)
         self.assertNotEqual(response.body.find('Math2'), -1)
         self.assertNotEqual(response.body.find('Math3'), -1)
