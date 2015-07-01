de479ac060d3d9ae71c461e11090c18200046cbd
Fedor Loktev <fmlokt@gmail.com>
2015-07-01 22:41:01 +0300

    First attempt to make Calendar API support, but help needed

--- service/calendar.py
+++ service/calendar.py
@@ -1,35 +1,72 @@
 # -*- coding: utf-8 -*-
 
+import os
+import httplib2
 import oauth2client
-from oauth2client.client import SignedJwtAssertionCredentials
+from google.appengine.ext import webapp
+from oauth2client.appengine import AppAssertionCredentials
 from httplib2 import Http
+from apiclient import discovery
 from apiclient.discovery import build
-
-##\brief Добавить событие в календарь
-def add_cal_record():
-    client_email = '261993259110-1m85s0ct6iosj9571ck2682h7irivquu@developer.gserviceaccount.com'
-    with open("TheSchedule-9bd45c6dec1c.p12") as f:
-        private_key = f.read()
-
-    credentials = SignedJwtAssertionCredentials(client_email, private_key,
-        'https://www.googleapis.com/auth/sqlservice.admin')
-    http_auth = credentials.authorize(Http())
-    calendar = build('calendar', 'v3', http=http_auth)
-    event = {
-        'summary': 'Appointment',
-        'start': {
-            'dateTime': '2015-05-25T10:00:00.000+03:00'
-        },
-        'end': {
-            'dateTime': '2015-05-25T10:25:00.000+03:00'
-        },
-    }
-
-    created_event = service.events().insert(calendarId='primary', body=event).execute()
-    print created_event
+from handlers.basehandler import *
 
 ##\brief Календарь
-class Calendar(BaseLocalAdminHandler):
-    def get(self):
-        add_cal_record()
+    def CreateEvent(calendarId ,summary, start_time, end_time, description):
+        credentials = AppAssertionCredentials(
+            'https://www.googleapis.com/auth/calendar')
+        http_auth = credentials.authorize(Http())
+        calendar = build('calendar', 'v3', http=http_auth)
+        service = discovery.build('calendar', 'v3', http=http_auth)
+        event = {
+            'summary': summary,
+            'start': {
+                'dateTime': start_time
+            },
+            'end': {
+                'dateTime': end_time
+            },
+            'description': description
+        }
+
+        event = service.events().insert(calendarId=calendarId, body=event).execute()
+        return event.get('eventId')
+
+    def DeleteEvent(calendarId, eventId):
+        credentials = AppAssertionCredentials(
+            'https://www.googleapis.com/auth/calendar')
+        http_auth = credentials.authorize(Http())
+        calendar = build('calendar', 'v3', http=http_auth)
+        service = discovery.build('calendar', 'v3', http=http_auth)
+
+        service.events().delete(calendarId=calendarId, eventId=eventId).execute()
+
+    def CreateCalendar(group):
+        credentials = AppAssertionCredentials(
+            'https://www.googleapis.com/auth/calendar')
+        http_auth = credentials.authorize(Http())
+        calendar = build('calendar', 'v3', http=http_auth)
+        service = discovery.build('calendar', 'v3', http=http_auth)
+
+        calendar = {
+            'summary': group,
+            'timeZone': 'Europe/Moscow'
+        }
+
+        rule = {
+            'scope': {
+                'type': 'default' #this makes calendar public and free to read
+                },
+            'role': 'reader'
+            }
+
+        created_calendar = service.calendars().insert(body=calendar).execute()
+        calendarId = #Help needed
+        created_rule = service.acl().insert(calendarId='primary', body=rule).execute()
+        return calendarId
+
+    def DeleteCalendar(calendarId):
+        service.calendars().delete(calendarId).execute()
+
+
+
 
