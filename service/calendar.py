9ec455295a6978d1de3487dbb3795b9c737256ef
Fedor Loktev <fmlokt@gmail.com>
2015-07-02 18:25:47 +0300

    Added admin console on deploy for tests. Find out how to return event id.

--- service/calendar.py
+++ service/calendar.py
@@ -2,7 +2,8 @@
 
 import os
 import httplib2
-import oauth2client
+from oauth2client import *
+from apiclient import *
 from environment import JINJA_ENVIRONMENT
 from google.appengine.ext import webapp
 from oauth2client.appengine import AppAssertionCredentials
@@ -17,20 +18,19 @@ def CreateEvent(calendarId ,summary, start_time, end_time, description):
         'https://www.googleapis.com/auth/calendar')
     http_auth = credentials.authorize(Http())
     calendar = build('calendar', 'v3', http=http_auth)
-    service = discovery.build('calendar', 'v3', http=http_auth)
+    service = discovery.build('calendar', 'v3', http=http_auth) #это часть для auth, нужна везде
     event = {
         'summary': summary,
         'start': {
-            'dateTime': start_time + ':00+03:00'
+            'dateTime': start_time + ':00+03:00' #html возвращает в формате немного другом
         },
         'end': {
             'dateTime': end_time + ':00+03:00'
         },
         'description': description
     }
-
     event = service.events().insert(calendarId=calendarId, body=event).execute()
-    return event.get('eventId')
+    return event['id']
 
 def DeleteEvent(calendarId, eventId):
     credentials = AppAssertionCredentials(
@@ -55,15 +55,14 @@ def CreateCalendar(group):
 
     rule = {
         'scope': {
-            'type': 'default' #this makes calendar public and free to read
+            'type': 'default' #это настройки календаря, например сделать публичным для подписки
             },
         'role': 'reader'
         }
 
     created_calendar = service.calendars().insert(body=calendar).execute()
-    #calendarId =
     created_rule = service.acl().insert(calendarId='primary', body=rule).execute()
-    #return calendarId
+    return created_calendar['id']
 
 def DeleteCalendar(calendarId):
     service.calendars().delete(calendarId).execute()
@@ -82,8 +81,8 @@ class Calendar(BaseAdminHandler):
         start = self.request.get('start')
         end = self.request.get('end')
         desc = self.request.get('desc')
-        self.response.write(start + ' ; ' + end + ' ; ' + summary)
-        CreateEvent('4eff584ar3hrm3cbe014aup5qo@group.calendar.google.com', summary, start, end, desc)
+        event = CreateEvent('4eff584ar3hrm3cbe014aup5qo@group.calendar.google.com', summary, start, end, desc)
+        self.response.write('Event Id: ' + str(event))
 
 
 
