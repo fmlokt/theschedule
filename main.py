7119ff8bdf41685aacd0fbc4ee11031433c30cc6
Fedor Loktev <fmlokt@gmail.com>
2019-09-23 16:33:20 +0300

    Revert "minor"
    This reverts commit 393387386667f8a4299a12979b09a77d000bb32e.

--- main.py
+++ main.py
@@ -5,9 +5,6 @@ import os
 
 import jinja2
 import webapp2
-from flask import Flask
-
-app = Flask(__name__) 
 
 import environment
 from handlers import pairs
@@ -92,9 +89,3 @@ application = webapp2.WSGIApplication([
                   handler=pairs.GetJSON,
                   name='get json list')
 ], debug=True)
-
-
-app.add_url_rule("/","Home",groups.ChooseGroup)  
-
-if __name__ =="__main__":  
-    app.run(debug = True) 
